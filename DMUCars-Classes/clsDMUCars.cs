ab4b92a5f67240a417de9775b2c5f1e8146c7a8d
P2441751 <P2441751@GWHXX473WIN002W>
2020-02-10 15:33:37 +0000

    Adding Properties for the class.

--- DMUCars-Classes/clsDMUCars.cs
+++ DMUCars-Classes/clsDMUCars.cs
@@ -1,6 +1,14 @@
-﻿namespace DMUCars_Classes
+﻿using System;
+
+namespace DMUCars_Classes
 {
     public class clsDMUCars
     {
+        public string CarID { get; set; }
+        public string CarModel { get; set; }
+        public string CarBrand { get; set; }
+        public string CarColour { get; set; }
+        public int CarPrice { get; set; }
+        public DateTime CarYearProduced { get; set; }
     }
 }
--- DMUCarsTesting/tstDMUCars.cs
+++ DMUCarsTesting/tstDMUCars.cs
@@ -13,5 +13,55 @@ namespace DMUCarsTesting
             clsDMUCars ACar = new clsDMUCars();
             Assert.IsNotNull(ACar);
         }
+
+        [TestMethod]
+        public void CarIDPropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            string TestData = "MC323";
+            ACar.CarID = TestData;
+            Assert.AreEqual(ACar.CarID, TestData);
+        }
+        [TestMethod]
+        public void CarBrandPropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            string TestData = "Vauxhall";
+            ACar.CarBrand = TestData;
+            Assert.AreEqual(ACar.CarBrand, TestData);
+        }
+        [TestMethod]
+        public void CarModelPropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            string TestData = "Corsa";
+            ACar.CarModel = TestData;
+            Assert.AreEqual(ACar.CarModel, TestData);
+        }
+        [TestMethod]
+        public void CarColourPropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            string TestData = "Red";
+            Assert.IsNotNull(ACar);
+            ACar.CarColour = TestData;
+            Assert.AreEqual(ACar.CarColour, TestData);
+        }
+        [TestMethod]
+        public void CarPricePropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            Int32 TestData = 1;
+            ACar.CarPrice = TestData;
+            Assert.AreEqual(ACar.CarPrice, TestData);
+        }
+        [TestMethod]
+        public void CarYearProducedPropertyOK()
+        {
+            clsDMUCars ACar = new clsDMUCars();
+            DateTime TestData = DateTime.Now.Date;
+            ACar.CarYearProduced = TestData;
+            Assert.AreEqual(ACar.CarYearProduced, TestData);
+        }
     }
 }
