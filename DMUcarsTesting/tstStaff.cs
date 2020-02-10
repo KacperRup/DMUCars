6535d000ae97f13f5a6b3e734c188c0b8ff0523d
aasimsaina <aasim678@hotmail.co.uk>
2020-02-10 14:54:47 +0000

    adding testing and class for staff

---
+++ DMUcarsTesting/tstStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DMUCarsclasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMUcarsTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStaff DMUCars = new clsStaff();
+        }
+    }
+}
