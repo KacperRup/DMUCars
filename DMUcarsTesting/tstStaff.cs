2430a831300c4130ebaa49d3550657d93036741a
aasimsaina <aasim678@hotmail.co.uk>
2020-02-10 16:02:40 +0000

    added more testing for the tststaff

--- DMUcarsTesting/tstStaff.cs
+++ DMUcarsTesting/tstStaff.cs
@@ -8,9 +8,21 @@ namespace DMUcarsTesting
     public class tstStaff
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
+            //create an instance
             clsStaff DMUCars = new clsStaff();
+            // test to see that it exists
+            Assert.IsNotNull(DMUCars);
         }
+        [TestMethod]
+        public void StaffPropertyOK()
+        {
+            //create an instance of the class we wat to create 
+            clsStaff DMUCars = new clsStaff();
+            //create some test data to assign to the property 
+            
+        }
+
     }
 }
