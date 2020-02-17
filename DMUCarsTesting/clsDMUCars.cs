cdc476776d2ca0f58d7d09e883ab1d8986f28bdf
p2428763 <p2428763@my365.dmu.ac.uk>
2020-02-17 14:54:50 +0000

    Updated Properties

--- DMUCarsTesting/clsDMUCars.cs
+++ DMUCarsTesting/clsDMUCars.cs
@@ -5,5 +5,8 @@
         public string CarID { get; internal set; }
         public string CustomerNo { get; internal set; }
         public string DeliveryDate { get; internal set; }
+        public string OrderDate { get; internal set; }
+        public string OrderNo { get; internal set; }
+        public string StaffID { get; internal set; }
     }
 }
